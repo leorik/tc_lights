d3c13d5512c0e434bfdfed36e2bbf824e607485a
Leorik <Kardashoff@gmail.com>
2017-05-03 22:31:52 +0700

    App settings implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+#[macro_use]
+extern crate serde_derive;
+
 mod api_enquirer;
 mod settings;
 
 fn main() {
-
+    let settings = settings::read_settings();
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,15 +1,38 @@
-use std::io;
+extern crate serde_json;
 
+use std::io;
+use std::io::BufReader;
+use std::io::Read;
+use std::fs::File;
+
+use self::serde_json::{Value, Error};
+use super::serde_derive;
+
+#[derive(Serialize, Deserialize)]
 pub struct LightsSettings {
-    teamcity_url : String,
+    teamcity_url: String,
     projects: Vec<ProjectSettings>
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct ProjectSettings {
-    build_type_id : String,
-    pin_id : String
+    build_type_id: String,
+    pin_id: String
 }
 
 pub fn read_settings() -> Result<LightsSettings, io::Error> {
-    unimplemented!();
+    let json_settings = read_settings_file().unwrap();
+
+    let settings : LightsSettings = serde_json::from_str(&json_settings).unwrap();
+
+    Ok(settings)
+}
+
+fn read_settings_file() -> Result<String, io::Error> {
+    let file = File::open("lights.json")?;
+    let mut reader = BufReader::new(file);
+    let mut json_contents = String::new();
+    reader.read_to_string(&mut json_contents)?;
+
+    Ok(json_contents)
 }
